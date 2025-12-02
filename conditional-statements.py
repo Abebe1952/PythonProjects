a465e65cbec453294f53506e363f96fa986fee60
birha <birhan.nigus1905@gmail.com>
2025-12-02 16:43:07 -0500

    second commit

--- conditional-statements.py
+++ conditional-statements.py
@@ -1,2 +1,5 @@
 print("Welcome to the Treasure island!")
 print(" Your mission is to find the treasure")
+
+print(" Your mission is to find the treasure")
+print(" Your mission is to find the treasure")
